d9f94b4518dd0bfd2d34df38d1b9e4bb770f23d0
KKKHun2 <1224kim2son@naver.com>
2023-08-30 18:00:10 +0900

    ⚡api 받아오는거 수정

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,25 @@
-export interface IWeather {
-    id: number;
+
+
+import axios from 'axios';
+
+export const API_KEY = 'd189247ce8012abe147988569fccd6a8';
+
+export const getWeather = async (city: string): Promise<WeatherData> => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+  return response.data;
+};
+
+
+export interface WeatherData {
+    main: {
+      temp: number;
+      humidity: number;
+    };
+    weather: {
+      description: string;
+      icon: string;
+    }[];
     name: string;
   }
