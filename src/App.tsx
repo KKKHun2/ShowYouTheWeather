0902c2b4014c1acf5fe31e63598097b5a0d108ef
KKKHun2 <1224kim2son@naver.com>
2023-08-30 18:03:11 +0900

    ⚡라우터 추가 및 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Weather from './components/Weather';
+import CountryCity from './components/CountryCity';
+import CitySelector from './components/CitySelector';
 import './styles/globals.css';
 
 function App() {
   return (
-    <div className="bg-gray-400 h-full w-full p-10">
-      <h1 className="text-xl mt-4 ml-10 font-bold">Hello, Tailwind CSS and TypeScript!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Weather App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<CountryCity />} />
+            <Route path="/weather/:country" element={<CitySelector />} />
+            <Route path="/weather/:country/:city" element={<Weather />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
