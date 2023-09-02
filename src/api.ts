c3d41cd341ae9e771791488f1c07ad48ef7f94db
KKKHun2 <1224kim2son@naver.com>
2023-09-02 23:13:07 +0900

    ✨useQuery로 수정중

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,19 @@
 
 
-import axios from 'axios';
-
+const BASE_PATH = "https://api.openweathermap.org/data/2.5/weather";
 export const API_KEY = 'd189247ce8012abe147988569fccd6a8';
 
-export const getWeather = async (city: string): Promise<WeatherData> => {
-  const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-  );
-  return response.data;
+export function getWeather(city: string) {
+  return fetch( `${BASE_PATH}?q=${city}&appid=${API_KEY}&units=metric`).then(
+  (response) => response.json()
+  )
 };
+
+export function getCity(city: string) {
+  return fetch(`https://api.open-meteo.com/v1/forecast?city=${city}`).then(
+    (response) => response.json()
+  );
+}
 
 
 export interface WeatherData {
