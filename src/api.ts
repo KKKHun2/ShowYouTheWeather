ef3684f5fbd71b8eede8a2ecc1426a7ac416643d
KKKHun2 <1224kim2son@naver.com>
2023-09-02 23:40:26 +0900

    🐛주소 버그 수정

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,8 @@
 
 const BASE_PATH = "https://api.openweathermap.org/data/2.5/weather";
 export const API_KEY = 'd189247ce8012abe147988569fccd6a8';
+const BASE_PATH2 ="https://restcountries.com"
+
 
 export function getWeather(city: string) {
   return fetch( `${BASE_PATH}?q=${city}&appid=${API_KEY}&units=metric`).then(
@@ -15,6 +17,11 @@ export function getCity(city: string) {
   );
 }
 
+export function getCountry() {
+  return fetch(`${BASE_PATH2}/v3.1/all`).then(
+    (response) => response.json()
+  );
+}
 
 export interface WeatherData {
     main: {
