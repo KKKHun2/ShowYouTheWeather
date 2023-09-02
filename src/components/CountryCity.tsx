7dcae15f4e746e891e10ef63e047db3ddc075d9c
KKKHun2 <1224kim2son@naver.com>
2023-09-02 23:36:16 +0900

    🐛 나라 이름 안나오는거 나오게 수정

--- src/components/CountryCity.tsx
+++ src/components/CountryCity.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getCountry } from '../api';
@@ -6,15 +6,26 @@ import { getCountry } from '../api';
 export interface Country {
   name: string;
 }
-
 function CountryCity() {
-  const [countries, setCountries] = useState<Country[]>([]);
   const navigate = useNavigate();
+  const [countries, setCountries] = useState<Country[]>([]);
+
 
   const { data: countryData, isLoading } = useQuery<Country[]>(
-    ['cities'], getCountry
-  );;
- 
+    ['countries'],
+    getCountry
+  );
+
+  useEffect(() => {
+    if (!isLoading && countryData) {
+      const countryNames = countryData.map((country: any) => {
+        return {
+          name: country.name.common
+        };
+      });
+      setCountries(countryNames);
+    }
+  }, [isLoading, countryData]);
 
   const handleCountryClick = (country: string) => {
     navigate(`/weather/${country}`);
@@ -22,18 +33,24 @@ function CountryCity() {
 
   return (
     <div>
-      <h2>Select a country:</h2>
-      <ul>
-        {countryData.map((country, index) => (
-          <li key={index}>
-            <button onClick={() => handleCountryClick(country.name+"")}>
-              {country.name}
-            </button>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <h2>Select a country:</h2>
+          <ul>
+            {countries.map((country, index) => (
+              <li key={index}>
+                <button onClick={() => handleCountryClick(country.name)}>
+                  {country.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default CountryCity;
